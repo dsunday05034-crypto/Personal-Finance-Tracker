1433b7fd1f716f55c7cfcf452d7559ef7d55fdbe
dasunday <dsunday05034@gmail.com>
2026-09-07 10:40:02 +0100

    storing transaction in the state

--- components/ExpenseTracker.js
+++ components/ExpenseTracker.js
@@ -1,13 +1,27 @@
 "use client"
 import ExpenseCard from "@/components/ExpenseCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Logout from "@/components/Logout";
 import { supabase } from "@/lib/supabase";
 
 export default function ExpenseTracker(props) {
-    const [category, setCategory] = useState("")
-    const [amount, setAmount] = useState(0)
-    const [isSubmitting, setIsSubmitting] = useState(false)
+    const [category, setCategory] = useState("");
+    const [amount, setAmount] = useState(0);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [transactions, setTransactions] = useState([]);
+
+    useEffect(() => {
+        fetchTransaction();
+    }, []);
+
+    async function fetchTransaction() {
+        const { data, error } = await supabase.from("transactions").select("*");
+        if (error) {
+            console.log(error);
+        } else {
+            setTransactions(data)
+        }
+    }
 
     return (
         <div>
@@ -31,11 +45,14 @@ export default function ExpenseTracker(props) {
                     user_id: props.session.user.id,
                 });
                 setIsSubmitting(false)
+                fetchTransaction();
             }}>
                 Save
             </button>
 
-            <ExpenseCard category={category} amount={amount} />
+            {transactions.map((t) => (
+                <ExpenseCard key={t.id} category={t.category} amount={t.amount} />
+            ))}
             <Logout />
         </div>
     );
